32deb045de07b48332f768602d6faca82716d8bb
Paraczki Andrada <paraczkiandrada31@gmail.com>
2025-01-28 13:00:37 +0200

    added tailwind

---
+++ frontend/vite.config.js
@@ -0,0 +1,20 @@
+// vite.config.js
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/postcss';
+
+export default defineConfig({
+  plugins: [
+    react(),
+  ],
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss(),
+      ],
+    },
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+});
